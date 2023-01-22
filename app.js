76a76d02e9cfc98d5dc47d474bae441f247e8b18
AndreiSolomenko <andrsolo10@gmail.com>
2023-01-22 17:05:04 +0200

    js homework 3

--- app.js
+++ app.js
@@ -1,32 +1,53 @@
-function renderCurrencys(currencys) {
+function createStorage() {
+  let currenciesBackup = [];
 
-  let htmlStr = currencys.map(function(currency) {
-    return `<tr>
+  return {
+    getCurrencies: function() {
+      return currenciesBackup;
+    },
+    setCurrencies: function(newCurrencies) {
+      currenciesBackup = newCurrencies;
+    }
+  }
+}
+
+const storage = createStorage();
+
+function renderCurrencies(currencies) {
+  let htmlStr = currencies.reduce(function(acc, currency, r030) {
+    return acc + `<tr>
       <td>${currency.r030}</td>
       <td>${currency.txt}</td>
-      <td>${currency.rate}</td>
+      <td>${currency.rate.toFixed(2)}</td>
       <td>${currency.cc}</td>
       <td>${currency.exchangedate}</td>
  </tr>`;
-  }).join('');
+  }, '');
 
-  document.getElementById('currencys-tbody').innerHTML = htmlStr;
+  document.getElementById('currencies-tbody').innerHTML = htmlStr;
 }
 
-fetch('https://bank.gov.ua/NBUStatService/v1/statdirectory/exchange?date=20230117&json').then(function(data) {
+function getData(date) {
+  fetch('https://bank.gov.ua/NBUStatService/v1/statdirectory/exchange?date=' + date + '&json').then(function(data) {
   return data.json();
-}).then(function(data) {
+  }).then(function(data) {
+  storage.setCurrencies(data);
+  renderCurrencies(data);
+  })
 
-  const currencys = data.map(function(currency) {
-    return {
-      r030: currency?.r030,
-      txt: currency?.txt,
-      rate: currency?.rate.toFixed(2),
-      cc: currency?.cc,
-      exchangedate: currency?.exchangedate
-    }
-  });
+}
 
-  renderCurrencys(currencys);
+getData('20230117');
 
-})
+document.getElementById('searchDate').onchange = function(e) {
+  getData(e.currentTarget.value.replaceAll('-', ''));
+}
+
+document.getElementById('search').onkeyup = function(e) {
+  const currentSearch = e.currentTarget.value.toLowerCase().trim();
+  const backup = storage.getCurrencies();
+  const filteredCurrencies = backup.filter(function(currency) {
+    return currency.txt.toLowerCase().includes(currentSearch);
+  })
+  renderCurrencies(filteredCurrencies);
+}
